347966cf981e7f2e7a5f6842e28623a5b4584f0b
mnurullahsaglam <nurullahsl87@gmail.com>
2024-01-09 23:28:56 +0300

    update admin user name and email

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::factory()->create([
-            'name' => 'Admin User',
-            'email' => 'admin@admin.com',
+            'name' => 'Mehmet Nurullah Sağlam',
+            'email' => 'nurullahsl87@gmail.com',
         ]);
 
         $this->call([
